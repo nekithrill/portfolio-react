18b2a05c6fb0d771227eaeceea6fa426adf2daa6
nekithrill <nemnogoznaal@gmail.com>
2025-07-20 19:05:47 +0300

    feat: i18n init + localization files

--- src/app/config/i18n/i18n.ts
+++ src/app/config/i18n/i18n.ts
@@ -1,24 +1,23 @@
 import enTranslation from '@/app/config/i18n/locales/en.json'
 import ruTranslation from '@/app/config/i18n/locales/ru.json'
 import uaTranslation from '@/app/config/i18n/locales/ua.json'
+import {
+	LANGUAGES,
+	type Language
+} from '@/features/language-switch/LanguageSwitch.types'
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-i18n.use(initReactI18next).init({
-	resources: {
-		en: {
-			translation: enTranslation
-		},
-		ru: {
-			translation: ruTranslation
-		},
-		uk: {
-			translation: uaTranslation
-		}
-	},
+const resources: Record<Language, { translation: object }> = {
+	[LANGUAGES.EN]: { translation: enTranslation },
+	[LANGUAGES.RU]: { translation: ruTranslation },
+	[LANGUAGES.UK]: { translation: uaTranslation }
+}
 
-	lng: 'en',
-	fallbackLng: 'en',
+i18n.use(initReactI18next).init({
+	resources,
+	lng: LANGUAGES.EN,
+	fallbackLng: LANGUAGES.EN,
 	interpolation: {
 		escapeValue: false
 	}
