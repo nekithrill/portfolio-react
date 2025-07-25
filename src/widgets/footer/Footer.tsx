84f862363278f3739ecaf2be64bf7824c0fc35f5
nekithrill <nemnogoznaal@gmail.com>
2025-07-25 17:23:38 +0300

    refactor(footer): added span for nickname, added some css rules

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -4,7 +4,9 @@ export const Footer = () => {
 	return (
 		<footer className={styles['footer']}>
 			<div className={styles['footer__content']}>
-				<p>&copy; 2025 Nekithrill. All rights reserved. </p>
+				<p>
+					&copy; 2025 <span>Nekithrill</span>. All rights reserved.{' '}
+				</p>
 				<div className={styles['footer__links']}>
 					<a href='#'>Privacy</a>
 					<a href='#'>Terms</a>
