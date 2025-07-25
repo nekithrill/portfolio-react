156dfb485477f718ad9a99c172066009979ea37d
nekithrill <nemnogoznaal@gmail.com>
2025-07-25 17:22:41 +0300

    Logo component moved from header to icons

--- src/shared/components/icons/Logo.tsx
+++ src/shared/components/icons/Logo.tsx
@@ -1,5 +1,5 @@
 import logo from '@/shared/assets/logo.svg'
-import styles from './Header.module.scss'
+import styles from '@/widgets/header/Header.module.scss'
 
 export const Logo = () => (
 	<img src={logo} className={styles['logo']} alt='Logo' />
