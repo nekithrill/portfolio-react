3932e255f5ba925cb4c9c9a99efa60e138c67551
nekithrill <nemnogoznaal@gmail.com>
2025-07-25 17:24:54 +0300

    refactor(header): imports renamed, little css changes

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,8 +1,8 @@
-import { DigitalClock } from '@/features/digital-clock/DigitalClock'
-import { LanguageSwitcher } from '@/features/language-switcher/LanguageSwitcher'
-import { ThemeSwitcher } from '@/features/theme-switcher/ThemeSwitcher'
+import { Clock } from '@/features/clock/Clock'
+import { LanguageSwitch } from '@/features/language-switch/LanguageSwitch'
+import { ThemeSwitch } from '@/features/theme-switch/ThemeSwitch'
+import { Logo } from '@/shared/components/icons/Logo'
 import styles from './header.module.scss'
-import { Logo } from './Logo'
 
 export const Header = () => {
 	return (
@@ -11,11 +11,11 @@ export const Header = () => {
 				<Logo />
 			</div>
 			<div className={styles['header__clock']}>
-				<DigitalClock />
+				<Clock />
 			</div>
 			<div className={styles['header__settings']}>
-				<LanguageSwitcher />
-				<ThemeSwitcher />
+				<LanguageSwitch />
+				<ThemeSwitch />
 			</div>
 		</header>
 	)
