4b6e7b4d1401e7f28343998fd663dd512ffdcfe6
nekithrill <nemnogoznaal@gmail.com>
2025-08-07 18:26:35 +0300

    feat(clock): changed day format (long -> short)

--- src/features/clock/Clock.types.ts
+++ src/features/clock/Clock.types.ts
@@ -7,7 +7,7 @@ export const DATE_TIME_OPTIONS = {
 		second: '2-digit' as const
 	},
 	date: {
-		weekday: 'long' as const,
+		weekday: 'short' as const,
 		year: 'numeric' as const,
 		month: 'long' as const,
 		day: 'numeric' as const
