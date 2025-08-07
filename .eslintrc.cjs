3972bfad34b24775daa761870725f7e63da5506c
nekithrill <nemnogoznaal@gmail.com>
2025-08-07 11:33:51 +0300

    chore(ESlint): config finished (added rules, plugins and others)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,35 @@
 module.exports = {
 	root: true,
 	env: { browser: true, es2020: true },
+	parser: '@typescript-eslint/parser',
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
 		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
 		'prettier'
 	],
+	parserOptions: {
+		ecmaFeatures: { jsx: true },
+		ecmaVersion: 2020,
+		sourceType: 'module'
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
+	plugins: [
+		'react',
+		'react-refresh',
+		'react-hooks',
+		'@typescript-eslint',
+		'@eslint-react'
+	],
 	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'@eslint-react/hooks-extra/no-direct-set-state-in-use-effect': 'warn',
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true }
