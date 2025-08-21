cb1557b10c48608f5265a9d7cffeea9b42bd49f3
nekithrill <nemnogoznaal@gmail.com>
2025-08-21 17:24:13 +0300

    refactor(LanguageSwitch): little changes

--- src/features/language-switch/useLanguageSwitch.ts
+++ src/features/language-switch/useLanguageSwitch.ts
@@ -5,11 +5,17 @@ import { LANGUAGES, LOCAL_STORAGE_LANGUAGE_KEY } from './LanguageSwitch.types'
 
 export default function useLanguageSwitch(): UseLanguageSwitchResult {
 	const { i18n } = useTranslation()
+
 	const [selectedLanguage, setSelectedLanguage] = useState<Language>(() => {
 		const lang = localStorage.getItem(
 			LOCAL_STORAGE_LANGUAGE_KEY
 		) as Language | null
-		return lang && Object.values(LANGUAGES).includes(lang) ? lang : LANGUAGES.EN
+		return lang &&
+			Object.values(LANGUAGES)
+				.map(l => l.value)
+				.includes(lang)
+			? lang
+			: LANGUAGES.EN.value
 	})
 
 	useEffect(() => {
@@ -17,9 +23,5 @@ export default function useLanguageSwitch(): UseLanguageSwitchResult {
 		localStorage.setItem(LOCAL_STORAGE_LANGUAGE_KEY, selectedLanguage)
 	}, [selectedLanguage, i18n])
 
-	const changeLanguage = (language: Language) => {
-		setSelectedLanguage(language)
-	}
-
-	return { selectedLanguage, changeLanguage }
+	return { selectedLanguage, changeLanguage: setSelectedLanguage }
 }
