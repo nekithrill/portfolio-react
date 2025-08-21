2709672740972d20f9de0a66a1d9ed4743379ebb
nekithrill <nemnogoznaal@gmail.com>
2025-08-21 17:29:14 +0300

    refactor(footer): improved footer layout

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -4,13 +4,17 @@ export const Footer = () => {
 	return (
 		<footer className={styles['footer']}>
 			<div className={styles['footer__content']}>
-				<p>
-					&copy; 2025 <span>Nekithrill</span>. All rights reserved.{' '}
-				</p>
-				<div className={styles['footer__links']}>
-					<a href='#'>Privacy</a>
-					<a href='#'>Terms</a>
-					<a href='#'>Cookie</a>
+				<div className={styles['footer__copyright']}>
+					<p>
+						&copy; 2025 <span>Nekithrill</span>.
+					</p>
+				</div>
+
+				<div className={styles['footer__hint']}>
+					<p>
+						<span>Scroll </span>
+						<span>to explore ⟶</span>
+					</p>
 				</div>
 			</div>
 		</footer>
