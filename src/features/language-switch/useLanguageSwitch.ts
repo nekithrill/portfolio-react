e09913f5be31ea7eb16462c0306e282b4221d313
nekithrill <nemnogoznaal@gmail.com>
2025-09-07 16:38:21 +0300

    feat(language-switch): hook moved to language provider

--- src/features/language-switch/useLanguageSwitch.ts
+++ src/features/language-switch/useLanguageSwitch.ts
@@ -1,27 +1,12 @@
-import { useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import type { Language, UseLanguageSwitchResult } from './LanguageSwitch.types'
-import { LANGUAGES, LOCAL_STORAGE_LANGUAGE_KEY } from './LanguageSwitch.types'
+import { useLanguage } from '@/app/providers/language/LanguageProvider'
+import type { Language } from './LanguageSwitch.types'
 
-export default function useLanguageSwitch(): UseLanguageSwitchResult {
-	const { i18n } = useTranslation()
+export const useLanguageSwitch = () => {
+	const { language, setLanguage } = useLanguage()
 
-	const [selectedLanguage, setSelectedLanguage] = useState<Language>(() => {
-		const lang = localStorage.getItem(
-			LOCAL_STORAGE_LANGUAGE_KEY
-		) as Language | null
-		return lang &&
-			Object.values(LANGUAGES)
-				.map(l => l.value)
-				.includes(lang)
-			? lang
-			: LANGUAGES.EN.value
-	})
+	const changeLanguage = (lang: Language) => {
+		setLanguage(lang)
+	}
 
-	useEffect(() => {
-		i18n.changeLanguage(selectedLanguage)
-		localStorage.setItem(LOCAL_STORAGE_LANGUAGE_KEY, selectedLanguage)
-	}, [selectedLanguage, i18n])
-
-	return { selectedLanguage, changeLanguage: setSelectedLanguage }
+	return { selectedLanguage: language, changeLanguage }
 }
