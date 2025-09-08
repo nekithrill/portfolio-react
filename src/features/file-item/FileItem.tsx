5b3b9dfb34134766d82bcecdd8abbd795b67512b
nekithrill <nemnogoznaal@gmail.com>
2025-09-08 17:46:14 +0300

    style(FileItem): changed icon size

--- src/features/file-item/FileItem.tsx
+++ src/features/file-item/FileItem.tsx
@@ -1,7 +1,7 @@
 import { DownloadIcon } from '@/shared/components/icons/DownloadIcon'
+import { FileIcon } from '@/shared/components/icons/FileIcon'
 import { IconWrapper } from '@/shared/components/icons/icon-wrapper/IconWrapper'
 import { InspectIcon } from '@/shared/components/icons/InspectIcon'
-import { PdfFileIcon } from '@/shared/components/icons/PdfFileIcon'
 import React from 'react'
 import styles from './FileItem.module.scss'
 import { FileItemProps } from './FileItem.types'
@@ -15,8 +15,8 @@ export const FileItem: React.FC<FileItemProps> = ({
 		<div className={styles['fileItem']}>
 			<div className={styles['fileItem__icon']}>
 				{icon || (
-					<IconWrapper size={30}>
-						<PdfFileIcon />
+					<IconWrapper size={20}>
+						<FileIcon />
 					</IconWrapper>
 				)}
 			</div>
@@ -30,12 +30,12 @@ export const FileItem: React.FC<FileItemProps> = ({
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					<IconWrapper size={28}>
+					<IconWrapper size={18}>
 						<InspectIcon />
 					</IconWrapper>
 				</a>
 				<a className={styles['fileItem__button']} href={fileUrl} download>
-					<IconWrapper size={28}>
+					<IconWrapper size={18}>
 						<DownloadIcon />
 					</IconWrapper>
 				</a>
