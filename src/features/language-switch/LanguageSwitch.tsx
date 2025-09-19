990f9df36b168dd60248dd1dbb0fa7394ec94745
nekithrill <nemnogoznaal@gmail.com>
2025-09-19 14:40:30 +0300

    refactor(language-switch): changed hook usage from separate hook to LanguageProvider

--- src/features/language-switch/LanguageSwitch.tsx
+++ src/features/language-switch/LanguageSwitch.tsx
@@ -1,18 +1,18 @@
+import { useLanguage } from '@/app/providers/language/LanguageProvider'
 import styles from './LanguageSwitch.module.scss'
 import { LANGUAGES } from './LanguageSwitch.types'
-import { useLanguageSwitch } from './useLanguageSwitch'
 
 export const LanguageSwitch = () => {
-	const { selectedLanguage, changeLanguage } = useLanguageSwitch()
+	const { language, setLanguage } = useLanguage()
 
 	return (
 		<div className={styles['language-switcher']}>
 			{Object.values(LANGUAGES).map(lang => (
 				<button
 					key={lang.value}
-					onClick={() => changeLanguage(lang.value)}
+					onClick={() => setLanguage(lang.value)}
 					className={`${styles['language-switcher__button']} ${
-						lang.value === selectedLanguage ? styles['active'] : ''
+						lang.value === language ? styles['active'] : ''
 					}`}
 				>
 					{lang.label}
