2e375bf952e5dd52ed62b10af1348274f48947d1
nekithrill <nemnogoznaal@gmail.com>
2025-09-19 14:42:13 +0300

    feat(theme-switch): added system icon + little changes due to hook deletion

--- src/features/theme-switch/ThemeSwitch.tsx
+++ src/features/theme-switch/ThemeSwitch.tsx
@@ -2,25 +2,33 @@ import { useTheme } from '@/app/providers/theme/ThemeProvider'
 import { IconWrapper } from '@/shared/components/icons/icon-wrapper/IconWrapper'
 import { MoonIcon } from '@/shared/components/icons/MoonIcon'
 import { SunIcon } from '@/shared/components/icons/SunIcon'
+import { SystemIcon } from '@/shared/components/icons/SystemIcon'
 import styles from './ThemeSwitch.module.scss'
 
 export const ThemeSwitch = () => {
-	const { isDarkTheme, toggleTheme } = useTheme()
+	const { theme, setTheme } = useTheme()
 
 	const buttons = [
 		{
+			icon: <SystemIcon />,
+			size: 18,
+			isActive: theme === 'system',
+			label: 'Switch to system theme',
+			onClick: () => setTheme('system')
+		},
+		{
 			icon: <SunIcon svgClassName={styles['sun-icon']} />,
-			size: 19,
-			isActive: !isDarkTheme,
+			size: 18,
+			isActive: theme === 'light',
 			label: 'Switch to light theme',
-			onClick: () => isDarkTheme && toggleTheme()
+			onClick: () => setTheme('light')
 		},
 		{
 			icon: <MoonIcon svgClassName={styles['moon-icon']} />,
-			size: 17,
-			isActive: isDarkTheme,
+			size: 18,
+			isActive: theme === 'dark',
 			label: 'Switch to dark theme',
-			onClick: () => !isDarkTheme && toggleTheme()
+			onClick: () => setTheme('dark')
 		}
 	]
 
