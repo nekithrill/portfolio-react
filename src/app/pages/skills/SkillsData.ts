96bc46a5b633d49f02cef004026c86f76aae6ba4
nekithrill <nemnogoznaal@gmail.com>
2025-09-19 14:26:48 +0300

    feat(pages-data): init data for each page

---
+++ src/app/pages/skills/SkillsData.ts
@@ -0,0 +1,65 @@
+export const skillsData = {
+	hardSkills: [
+		{
+			heading: 'section.skills.hard.programmingLanguages',
+			items: [
+				{ name: 'JavaScript', level: 2 },
+				{ name: 'TypeScript', level: 2 }
+			]
+		},
+		{
+			heading: 'section.skills.hard.frontend',
+			items: [
+				{ name: 'HTML', level: 3 },
+				{ name: 'CSS', level: 3 },
+				{ name: 'SCSS', level: 2 },
+				{ name: 'React', level: 2 },
+				{ name: 'Next.js', level: 2 },
+				{ name: 'Redux', level: 1 },
+				{ name: 'Zustand', level: 1 }
+			]
+		},
+		{
+			heading: 'section.skills.hard.backend',
+			items: [
+				{ name: 'Node.js', level: 2 },
+				{ name: 'Express.js', level: 2 },
+				{ name: 'NestJS', level: 1 }
+			]
+		},
+		{
+			heading: 'section.skills.hard.other',
+			items: [
+				{ name: 'Vite', level: 2 },
+				{ name: 'Webpack', level: 2 },
+				{ name: 'Git', level: 2 },
+				{ name: 'Docker', level: 1 }
+			]
+		}
+	],
+
+	softSkills: [
+		'section.skills.soft.communication',
+		'section.skills.soft.problemSolving',
+		'section.skills.soft.adaptability',
+		'section.skills.soft.creativity',
+		'section.skills.soft.enthusiasm'
+	],
+
+	principles: ['SOLID', 'DRY', 'KISS', 'YAGNI'],
+
+	languages: [
+		{
+			key: 'section.skills.languages.english',
+			level: 'b1'
+		},
+		{
+			key: 'section.skills.languages.ukrainian',
+			level: 'section.skills.languages.native'
+		},
+		{
+			key: 'section.skills.languages.russian',
+			level: 'section.skills.languages.native'
+		}
+	]
+}
