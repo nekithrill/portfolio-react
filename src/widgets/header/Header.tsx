4d90e04b2dee24aa87e8737e07a48e4e43347504
nekithrill <nemnogoznaal@gmail.com>
2025-09-19 14:46:53 +0300

    refactor(header): header layout reworked and improved

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,23 +1,17 @@
 import { Clock } from '@/features/clock/Clock'
 import { LanguageSwitch } from '@/features/language-switch/LanguageSwitch'
 import { ThemeSwitch } from '@/features/theme-switch/ThemeSwitch'
-import { Logo } from '@/shared/components/icons/Logo'
 import styles from './header.module.scss'
 
 export const Header = () => {
 	return (
 		<header className={styles['header']}>
-			<div className={styles['header__content']}>
-				<div className={styles['header__logo']}>
-					<Logo />
-				</div>
-				<div className={styles['header__clock']}>
-					<Clock />
-				</div>
-				<div className={styles['header__settings']}>
-					<ThemeSwitch />
-					<LanguageSwitch />
-				</div>
+			<div className={styles['header__clock']}>
+				<Clock />
+			</div>
+			<div className={styles['header__settings']}>
+				<ThemeSwitch />
+				<LanguageSwitch />
 			</div>
 		</header>
 	)
