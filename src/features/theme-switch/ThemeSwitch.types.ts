a907a373b2cc42207998008e61f79917c95e4bc2
nekithrill <nemnogoznaal@gmail.com>
2025-09-19 14:43:29 +0300

    refactor(theme-switch): created new type Theme, edited properties of interface

--- src/features/theme-switch/ThemeSwitch.types.ts
+++ src/features/theme-switch/ThemeSwitch.types.ts
@@ -2,7 +2,9 @@ export const THEME_DARK_CLASS = 'dark-theme'
 export const THEME_LIGHT_CLASS = 'light-theme'
 export const LOCAL_STORAGE_THEME_KEY = 'theme'
 
+export type Theme = 'system' | 'light' | 'dark'
+
 export interface ThemeSwitchResult {
-	isDarkTheme: boolean
-	toggleTheme: () => void
+	theme: Theme
+	setTheme: (theme: Theme) => void
 }
