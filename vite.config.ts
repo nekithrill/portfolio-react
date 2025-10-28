47c974c42b3829041c05bcdebb4522c2be22a0a3
nekithrill <nekithrill@gmail.com>
2025-10-28 02:30:26 +0200

    chore(vite.config): added aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,18 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	base: '/',
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@dictionaries': path.resolve(__dirname, './dictionaries')
+		}
+	},
 	server: {
 		port: Number(process.env.VITE_PORT) || 3000,
 		host: true,
 		watch: {
 			usePolling: true
-		}
-	},
-
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src')
 		}
 	}
 })
