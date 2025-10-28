3486259be6bc61f7a531a7ebef63d89f2557f341
nekithrill <nekithrill@gmail.com>
2025-10-28 13:31:38 +0200

    feat(home page): added data, reworked layout

--- src/app/pages/home/HomeData.ts
+++ src/app/pages/home/HomeData.ts
@@ -1,3 +1,38 @@
-export const homeData = {
-	features: []
-}
+export const homeData = [
+	{
+		useH1: true,
+		title: 'section.home.title',
+		text: 'section.home.text',
+		className: 'home__intro'
+	}
+	// {
+	// 	useH1: false,
+	// 	title: 'section.home.featuresTitle',
+	// 	text: 'section.home.featuresText',
+	// 	className: 'home__intro'
+	// },
+	// {
+	// 	useH1: false,
+	// 	title: 'section.home.featuresTitle',
+	// 	text: 'section.home.featuresText',
+	// 	className: 'home__intro'
+	// },
+	// {
+	// 	useH1: false,
+	// 	title: 'section.home.featuresTitle',
+	// 	text: 'section.home.featuresText',
+	// 	className: 'home__intro'
+	// },
+	// {
+	// 	useH1: false,
+	// 	title: 'section.home.featuresTitle',
+	// 	text: 'section.home.featuresText',
+	// 	className: 'home__intro'
+	// },
+	// {
+	// 	useH1: false,
+	// 	title: 'section.home.featuresTitle',
+	// 	text: 'section.home.featuresText',
+	// 	className: 'home__intro'
+	// }
+]
