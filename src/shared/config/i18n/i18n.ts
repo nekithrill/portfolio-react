015c4a10b7ab0df0a67c6ea7bf95c2d735e7faf2
nekithrill <nekithrill@gmail.com>
2025-10-28 02:32:15 +0200

    chore(folder-structure): moved config folder to /shared

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,10 +1,10 @@
-import enTranslation from '@/app/config/i18n/locales/en.json'
-import ruTranslation from '@/app/config/i18n/locales/ru.json'
-import uaTranslation from '@/app/config/i18n/locales/ua.json'
 import {
 	LANGUAGES,
 	type Language
 } from '@/features/language-switch/LanguageSwitch.types'
+import enTranslation from '@dictionaries/en.json'
+import ruTranslation from '@dictionaries/ru.json'
+import uaTranslation from '@dictionaries/ua.json'
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
