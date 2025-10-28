28db15733772400cccc4051ab3caf81b1991f53d
nekithrill <nekithrill@gmail.com>
2025-10-28 13:18:19 +0200

    feat(footer): added translations usage

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,16 +1,24 @@
 import { Logo } from '@/shared/components/icons/Logo'
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
 import styles from './Footer.module.scss'
 
 export const Footer = () => {
+	const { t } = useTranslation()
+
 	return (
 		<footer className={styles['footer']}>
 			<div className={styles['footer__logo']}>
 				<Logo />
 			</div>
 
-			<p>
-				&copy; 2025 <span>Nekithrill</span>.
-			</p>
+			<div className={styles['footer__text']}>
+				<Link to='/privacy'>{t('menu.privacy')}</Link>
+
+				<p>
+					&copy; 2025 <span>Nekithrill</span>.
+				</p>
+			</div>
 		</footer>
 	)
 }
