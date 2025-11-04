56160a5ad5e7d12fbd99f97150e937ded0965de2
nekithrill <nekithrill@gmail.com>
2025-11-04 15:41:03 +0200

    feat(contacts-page): added email icon to data, added page styling, removed feedback form and improved layout

--- src/app/pages/contacts/ContactsData.ts
+++ src/app/pages/contacts/ContactsData.ts
@@ -1,21 +1,29 @@
 import { DiscordIcon } from '@/shared/components/icons/DiscordIcon'
 import { GithubIcon } from '@/shared/components/icons/GithubIcon'
+import { MailIcon } from '@/shared/components/icons/MailIcon'
 import { TelegramIcon } from '@/shared/components/icons/TelegramIcon'
 
-export const contactsData = [
-	{
-		name: 'GitHub',
-		url: 'https://github.com/nekithrill',
-		icon: GithubIcon
-	},
-	{
-		name: 'Discord',
-		url: 'http://discord.com/users/342779325151444993',
-		icon: DiscordIcon
-	},
-	{
-		name: 'Telegram',
-		url: 'https://t.me/nekithrill',
-		icon: TelegramIcon
-	}
-]
+export const contactsData = {
+	icons: [
+		{
+			name: 'GitHub',
+			url: 'https://github.com/nekithrill',
+			icon: GithubIcon
+		},
+		{
+			name: 'Discord',
+			url: 'http://discord.com/users/342779325151444993',
+			icon: DiscordIcon
+		},
+		{
+			name: 'Telegram',
+			url: 'https://t.me/nekithrill',
+			icon: TelegramIcon
+		},
+		{
+			name: 'Email',
+			url: 'mailto:nekithrill@gmail.com',
+			icon: MailIcon
+		}
+	]
+}
--- src/app/pages/contacts/ContactsPage.tsx
+++ src/app/pages/contacts/ContactsPage.tsx
@@ -1,4 +1,3 @@
-import { ContactForm } from '@/features/contact-form/ContactForm'
 import { IconWrapper } from '@/shared/components/icons/icon-wrapper/IconWrapper'
 import Section from '@/shared/components/ui/section/Section'
 import { useTranslation } from 'react-i18next'
@@ -7,6 +6,7 @@ import styles from './ContactsPage.module.scss'
 
 export function ContactsPage() {
 	const { t } = useTranslation('')
+	const { icons } = contactsData
 
 	return (
 		<div className={styles['contacts']}>
@@ -14,32 +14,26 @@ export function ContactsPage() {
 				useH1={true}
 				title='section.contacts.title'
 				className={styles['contacts__text']}
+				footerInfo={
+					<ul className={styles['contacts__socials']}>
+						{icons.map(({ name, url, icon: Icon }) => (
+							<li key={name} className={styles['item']}>
+								<a
+									href={url}
+									target='_blank'
+									rel='noopener noreferrer'
+									aria-label={name}
+								>
+									<IconWrapper width={48} height={32}>
+										<Icon />
+									</IconWrapper>
+								</a>
+							</li>
+						))}
+					</ul>
+				}
 			>
 				<p>{t('section.contacts.text')}</p>
-
-				<ul className={styles['contacts__socials']}>
-					{contactsData.map(({ name, url, icon: Icon }) => (
-						<li key={name} className={styles['item']}>
-							<a
-								href={url}
-								target='_blank'
-								rel='noopener noreferrer'
-								aria-label={name}
-							>
-								<IconWrapper width={40} height={40}>
-									<Icon />
-								</IconWrapper>
-							</a>
-						</li>
-					))}
-				</ul>
-			</Section>
-
-			<Section
-				title='section.contacts.form.title'
-				className={styles['contacts__form']}
-			>
-				<ContactForm />
 			</Section>
 		</div>
 	)
